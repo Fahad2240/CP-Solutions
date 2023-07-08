71b8d35951bcc5fdcb5906e003ac98fd44eabf93
Fahad2240 <81286322+Fahad2240@users.noreply.github.com>
2023-07-08 21:07:06 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc215_a/25362542.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+    if(s=="Hello,World!")
+        cout<<"AC"<<endl;
+    else
+        cout<<"WA"<<endl;
+}
